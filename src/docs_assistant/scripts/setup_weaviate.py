97dd778d4ae5e9eb18b2d0e308061f06b3631f01
Sheng Han Lim <shenghan@gmail.com>
2023-08-01 16:34:18 +0800

    add weaviate setup script

---
+++ src/docs_assistant/scripts/setup_weaviate.py
@@ -0,0 +1,22 @@
+import weaviate
+import argparse
+
+def restore_data(backup_id: str,
+                url="http://localhost:8080") -> None:
+
+    client = weaviate.Client(url)
+
+    result = client.backup.restore(
+        backup_id=backup_id,
+        backend='filesystem',
+)
+
+if __name__ == "__main__":
+    # example usage
+    # python backup_script.py your_backup_id_here --url http://localhost:8080"
+    parser = argparse.ArgumentParser(description="Setup weaviate client")
+    parser.add_argument("backup_id", type=str, help="Specify the backup ID as a string.")
+    parser.add_argument("--url", type=str, help="Specify weaviate URL for the backup (optional).")
+    args = parser.parse_args()
+
+    restore_data(args.backup_id, args.url)
