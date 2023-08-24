85624805e77cc150714072623ea11daab6c502be
Sheng Han Lim <shenghan@gmail.com>
2023-08-24 22:07:23 +0800

    wip setup python env only if doesn't exist

--- scripts/post_install.sh
+++ scripts/post_install.sh
@@ -2,26 +2,15 @@
 DATESTAMP="$(date +%FT%H:%m)"
 
 CD_INSTALL_TARGET=/home/ubuntu/datagovmy-ai-deploy
-DEPLOY_TEMP=/home/ubuntu/deploy-tmp
-DOCS_API_ROOT=${CD_INSTALL_TARGET}/src/docs_assistant
-DOCS_API_ENV=${DOCS_API_ROOT}/.env
-WEAVIATE_ENV=${DOCS_API_ROOT}/scripts/weaviate/.env
 
 # update permissions
 sudo chown -R ubuntu:ubuntu ${CD_INSTALL_TARGET}
 
-# setup python environment
-cd $CD_INSTALL_TARGET
-python -m venv $CD_INSTALL_TARGET/env
-source env/bin/activate
-pip install -r requirements.txt
-
-# restore .env files if exists
-# if [ -f ${DEPLOY_TEMP}/main.env.bak ]; then
-#     echo "[${DATESTAMP}] restoring .env"
-#     cp ${DEPLOY_TEMP}/main.env.bak ${DOCS_API_ENV}
-# if [ -f ${DEPLOY_TEMP}/weaviate.env.bak ]; then
-#     echo "[${DATESTAMP}] restoring weaviate.env"
-#     cp ${DEPLOY_TEMP}/weaviate.env.bak ${WEAVIATE_ENV}
+# setup python environment if doesn't exist
+if [ ! -d ${CD_INSTALL_TARGET}/env ]; then
+    cd $CD_INSTALL_TARGET
+    python -m venv $CD_INSTALL_TARGET/env
+    source env/bin/activate
+    pip install -r requirements.txt
 
 echo "[${DATESTAMP}] post install step completed"
