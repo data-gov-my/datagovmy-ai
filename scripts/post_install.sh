93a4f268ee5dfab7e00b4c6f60eb845d943bd2bb
Sheng Han Lim <shenghan@gmail.com>
2023-08-24 14:13:45 +0800

    wip switching back to CD and fixes to post install

--- scripts/post_install.sh
+++ scripts/post_install.sh
@@ -3,8 +3,8 @@ DATESTAMP="$(date +%FT%H:%m)"
 
 CD_INSTALL_TARGET=/home/ubuntu/datagovmy-ai-deploy
 
-cd ${CD_INSTALL_TARGET}
-python -m venv env
+cd $CD_INSTALL_TARGET
+python -m venv $CD_INSTALL_TARGET/env
 source env/bin/activate
 pip install -r requirements.txt
 
