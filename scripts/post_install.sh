a1727d6cc668980ca7619d8187cde344de2d3681
Sheng Han Lim <shenghan@gmail.com>
2023-08-24 23:37:43 +0800

    add preserving of env files

--- scripts/post_install.sh
+++ scripts/post_install.sh
@@ -2,16 +2,35 @@
 DATESTAMP="$(date +%FT%H:%m)"
 
 CD_INSTALL_TARGET=/home/ubuntu/datagovmy-ai-deploy
+DEPLOY_TEMP=/home/ubuntu/deploy-tmp
+DOCS_API_ROOT=${CD_INSTALL_TARGET}/src/docs_assistant
+DOCS_API_ENV=${DOCS_API_ROOT}/.env
+WEAVIATE_ENV=${DOCS_API_ROOT}/scripts/weaviate/.env
+BIN_FILE=${DOCS_API_ROOT}/key.bin
 
 # update permissions
 sudo chown -R ubuntu:ubuntu ${CD_INSTALL_TARGET}
 
 # setup python environment if doesn't exist
-if [ ! -d ${CD_INSTALL_TARGET}/env ]; then
+if [ ! -d "${CD_INSTALL_TARGET}/env" ]; then
     cd $CD_INSTALL_TARGET
     python -m venv $CD_INSTALL_TARGET/env
-    source env/bin/activate
-    pip install -r requirements.txt
+fi
+source env/bin/activate
+pip install -r requirements.txt
+
+# restore files if exists
+if [ -f ${DEPLOY_TEMP}/main.env.bak ]; then
+    echo "[${DATESTAMP}] restoring .env"
+    cp ${DEPLOY_TEMP}/main.env.bak ${DOCS_API_ENV}
+fi
+if [ -f ${DEPLOY_TEMP}/weaviate.env.bak ]; then
+    echo "[${DATESTAMP}] restoring weaviate.env"
+    cp ${DEPLOY_TEMP}/weaviate.env.bak ${WEAVIATE_ENV}
+fi
+if [ -f ${DEPLOY_TEMP}/key.bin ]; then
+    echo "[${DATESTAMP}] restoring key file"
+    cp ${DEPLOY_TEMP}/key.bin ${BIN_FILE}
 fi
 
 echo "[${DATESTAMP}] post install step completed"
--- scripts/pre_install.sh
+++ scripts/pre_install.sh
@@ -1,5 +1,24 @@
 #!/usr/bin/env bash
 DATESTAMP="$(date +%FT%H:%m)"
 CD_INSTALL_TARGET=/home/ubuntu/datagovmy-ai-deploy
+DEPLOY_TEMP=/home/ubuntu/deploy-tmp
+DOCS_API_ROOT=${CD_INSTALL_TARGET}/src/docs_assistant
+DOCS_API_ENV=${DOCS_API_ROOT}/.env
+WEAVIATE_ENV=${DOCS_API_ROOT}/scripts/weaviate/.env
+BIN_FILE=${DOCS_API_ROOT}/key.bin
+
+# preserve .env files from previous deployment
+if [ -f ${DOCS_API_ENV} ]; then
+    echo "[${DATESTAMP}] preserving .env file"
+    cp ${DOCS_API_ENV} ${DEPLOY_TEMP}/main.env.bak
+fi
+if [ -f ${WEAVIATE_ENV} ]; then
+    echo "[${DATESTAMP}] preserving weaviate .env file"
+    cp ${WEAVIATE_ENV} ${DEPLOY_TEMP}/weaviate.env.bak
+fi
+if [ -f ${BIN_FILE} ]; then
+    echo "[${DATESTAMP}] preserving key file"
+    cp ${BIN_FILE} ${DEPLOY_TEMP}/key.bin
+fi
 
 echo "[${DATESTAMP}] pre install step completed"
