c617fa23e39e3d92f05322300c7674fd688b0d55
Sheng Han Lim <shenghan@gmail.com>
2023-09-05 19:14:57 +0800

    add optional class name arg for partial weaviate restore

--- scripts/weaviate/setup_weaviate.py
+++ scripts/weaviate/setup_weaviate.py
@@ -2,21 +2,19 @@ import weaviate
 import argparse
 
 
-def restore_data(backup_id: str, url: str) -> None:
+def restore_data(backup_id: str, url: str, class_name: str = None) -> None:
     client = weaviate.Client(url)
 
     result = client.backup.restore(
-        backup_id=backup_id,
-        backend="filesystem",
+        backup_id=backup_id, backend="filesystem", include_classes=class_name
     )
 
 
-def backup_data(backup_id: str, url: str) -> None:
+def backup_data(backup_id: str, url: str, class_name: str = None) -> None:
     client = weaviate.Client(url)
 
     result = client.backup.create(
-        backup_id=backup_id,
-        backend="filesystem",
+        backup_id=backup_id, backend="filesystem", include_classes=class_name
     )
 
 
@@ -30,6 +28,12 @@ if __name__ == "__main__":
     parser.add_argument(
         "--url", type=str, help="Specify weaviate URL for the backup (optional)."
     )
+    parser.add_argument(
+        "--class_name",
+        type=str,
+        default=None,
+        help="Specify the class name (optional).",
+    )
 
     action_group = parser.add_mutually_exclusive_group(required=True)
     action_group.add_argument(
@@ -42,7 +46,7 @@ if __name__ == "__main__":
 
     if args.backup:
         print("Performing backup action for backup ID:", args.backup_id)
-        backup_data(args.backup_id, args.url)
+        backup_data(args.backup_id, args.url, args.class_name)
     elif args.restore:
         print("Performing restore action for backup ID:", args.backup_id)
-        restore_data(args.backup_id, args.url)
+        restore_data(args.backup_id, args.url, args.class_name)
